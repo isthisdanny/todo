176d0316b17e1ae5953be773b354af79dd924050
Danny <flydanny0511@gmail.com>
2021-09-03 00:29:04 +0900

    Make change on app.py to connect with DB

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql+pymysql://"+ os.environ['DB_USER'] + ":"
+                                         + os.environ['DB_PASSWORD']+ "@"
+                                         + os.environ['DB_HOST'] + ":3306/"
+                                         + os.environ['DB_NAME'])
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
